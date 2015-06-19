b6ec955e2624927897fd01f4a24fe860cbef8025
Vince Ruby <vsruby91@gmail.com>
2015-06-19 16:55:14 -0400

    Can now dispatch a command from request

--- src/LaravelTactician/Dispatcher.php
+++ src/LaravelTactician/Dispatcher.php
@@ -2,6 +2,9 @@
 
 namespace JildertMiedema\LaravelTactician;
 
+use ArrayAccess;
+use ReflectionClass;
+use ReflectionParameter;
 use League\Tactician\CommandBus;
 
 class Dispatcher
@@ -21,8 +24,74 @@ class Dispatcher
         $this->bus->handle($command);
     }
 
-    public function dispatchFrom()
+    /**
+     * Marshal a command and dispatch it
+     * 
+     * @param  mixed       $command
+     * @param  ArrayAccess $source
+     * @param  array       $extras
+     * 
+     * @return mixed
+     */
+    public function dispatchFrom($command, ArrayAccess $source, array $extras = [])
     {
-        //
+        return $this->dispatch($this->marshal($command, $source, $extras));
+    }
+
+    /**
+     * Marshal a command from the given array accessible object
+     * 
+     * @param  mixed       $command
+     * @param  ArrayAccess $source
+     * @param  array       $extras
+     * 
+     * @return mixed
+     */
+    protected function marshal($command, ArrayAccess $source, array $extras = [])
+    {
+        $injected   = [];
+        $reflection = new ReflectionClass($command);
+
+        if ($constructor = $reflection->getConstructor()) {
+
+            $injected = array_map(function ($parameter) use ($command, $source, $extras) {
+                return $this->getParameterValueForCommand($command, $source, $parameter, $extras);
+            }, $constructor->getParameters());
+
+        }
+
+        return $reflection->newInstanceArgs($injected);        
+    }
+
+    /**
+     * Get a parameter value for a marshaled command.
+     *
+     * @param  string              $command
+     * @param  ArrayAccess         $source
+     * @param  ReflectionParameter $parameter
+     * @param  array               $extras
+     * @return mixed
+     */
+    protected function getParameterValueForCommand($command, ArrayAccess $source, ReflectionParameter $parameter, array $extras = [])
+    {
+        if (array_key_exists($parameter->name, $extras)) {
+
+            return $extras[$parameter->name];
+
+        }
+
+        if (isset($source[$parameter->name])) {
+
+            return $source[$parameter->name];
+
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+
+            return $parameter->getDefaultValue();
+
+        }
+        
+        MarshalException::whileMapping($command, $parameter);
     }
 }
