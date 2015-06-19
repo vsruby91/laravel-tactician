cdbb3361e78a5479f368f22e80007f4c319d8515
Vince Ruby <vsruby91@gmail.com>
2015-06-19 17:08:42 -0400

    Add dispatch from to trait

--- src/LaravelTactician/DispatchesCommands.php
+++ src/LaravelTactician/DispatchesCommands.php
@@ -13,4 +13,9 @@ trait DispatchesCommands
     {
         app('tactician.dispatcher')->dispatch($command);
     }
+
+    protected function dispatchFrom($command, ArrayAccess $source, array $extras = [])
+    {
+    	app('tactician.dispatcher')->dispatchFrom($command, $source, $extras);
+    }
 }
